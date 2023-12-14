630b9cb42dfb4d6f406946ba0f135662e1063e22
jox <zincir_890@hotmail.com>
2023-12-14 20:17:36 +0300

    Added Mapping Profiles

---
+++ TRA.Services/AutoMapper/Profiles/CommentProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRA.Entities.Concrete;
+using TRA.Entities.Dtos;
+
+namespace TRA.Services.AutoMapper.Profiles
+{
+    public class CommentProfile : Profile
+    {
+        public CommentProfile()
+        {
+            CreateMap<CommentAddDto, Comment>()
+                .ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(x => DateTime.Now))
+                .ForMember(dest => dest.ModifiedDate, opt => opt.MapFrom(x => DateTime.Now))
+                .ForMember(dest => dest.ModifiedByName, opt => opt.MapFrom(x => x.CreatedByName))
+                .ForMember(dest => dest.IsActive, opt => opt.MapFrom(x => false));
+            CreateMap<CommentUpdateDto, Comment>()
+                .ForMember(dest => dest.ModifiedDate, opt => opt.MapFrom(x => DateTime.Now));
+            CreateMap<Comment, CommentUpdateDto>();
+        }
+    }
+}
---
+++ TRA.Services/AutoMapper/Profiles/PostProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRA.Entities.Concrete;
+using TRA.Entities.Dtos;
+
+namespace TRA.Services.AutoMapper.Profiles
+{
+    public class PostProfile : Profile
+    {
+        public PostProfile()
+        {
+            CreateMap<PostAddDto, Post>().ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(x => DateTime.Now));
+            CreateMap<PostUpdateDto, Post>().ForMember(dest => dest.ModifiedDate, opt => opt.MapFrom(x => DateTime.Now));
+            CreateMap<Post, PostUpdateDto>();
+        }
+    }
+}
