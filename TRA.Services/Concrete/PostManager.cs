6439c0c45d00958e70bacf49593892ac338720d9
jox <zincir_890@hotmail.com>
2023-12-19 06:15:40 +0300

    Finished PostManager

---
+++ TRA.Services/Concrete/PostManager.cs
@@ -0,0 +1,217 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRA.Data.Abstract;
+using TRA.Entities.Concrete;
+using TRA.Entities.Dtos;
+using TRA.Services.Abstract;
+using TRA.Services.Utilities;
+using TRA.Shared.Utilities.Results.Abstract;
+using TRA.Shared.Utilities.Results.ComplexTypes;
+using TRA.Shared.Utilities.Results.Concrete;
+
+namespace TRA.Services.Concrete
+{
+    public class PostManager : ManagerBase, IPostService
+    {
+        public PostManager(IUnitOfWork unitOfWork, IMapper mapper) : base(unitOfWork, mapper)
+        {
+        }
+
+        public async Task<IResult> AddAsync(PostAddDto postAddDto, string createdByName, int userId)
+        {
+            var post = Mapper.Map<Post>(postAddDto);
+            post.CreatedByName = createdByName;
+            post.ModifiedByName = createdByName;
+            post.UserId = userId;
+            await UnitOfWork.Posts.AddAsync(post);
+            await UnitOfWork.SaveAsync();
+            return new Result(ResultStatus.Success, Messages.Post.Add(post.Title));
+        }
+
+        public async Task<IResult> UpdateAsync(PostUpdateDto postUpdateDto, string modifiedByName)
+        {
+            var oldPost = await UnitOfWork.Posts.GetAsync(p => p.Id == postUpdateDto.Id);
+            var post = Mapper.Map<PostUpdateDto, Post>(postUpdateDto, oldPost);
+            post.ModifiedByName = modifiedByName;
+            await UnitOfWork.Posts.UpdateAsync(post);
+            await UnitOfWork.SaveAsync();
+            return new Result(ResultStatus.Success, Messages.Post.Update(post.Title));
+        }
+
+        public async Task<IDataResult<int>> CountAsync()
+        {
+            var postsCount = await UnitOfWork.Posts.CountAsync();
+            if (postsCount > -1)
+            {
+                return new DataResult<int>(ResultStatus.Success, postsCount);
+            }
+            else
+            {
+                return new DataResult<int>(ResultStatus.Error, $"Encountered an unexpected error.", -1);
+            }
+        }
+
+        public async Task<IDataResult<int>> CountByNonDeletedAsync()
+        {
+            var postsCount = await UnitOfWork.Posts.CountAsync(p => !p.IsDeleted);
+            if (postsCount > -1)
+            {
+                return new DataResult<int>(ResultStatus.Success, postsCount);
+            }
+            else
+            {
+                return new DataResult<int>(ResultStatus.Error, $"Encountered an unexpected error.", -1);
+            }
+        }
+
+        public async Task<IDataResult<PostDto>> GetAsync(int postId)
+        {
+            var post = await UnitOfWork.Posts.GetAsync(p => p.Id == postId, p => p.User, p => p.Category, p => p.Comments);
+            if (post != null)
+            {
+                return new DataResult<PostDto>(ResultStatus.Success, new PostDto
+                {
+                    Post = post,
+                    ResultStatus = ResultStatus.Success
+                });
+            }
+            return new DataResult<PostDto>(ResultStatus.Error, Messages.Post.NotFound(isPlural: false), null);
+        }
+
+        public async Task<IDataResult<PostListDto>> GetAllAsync()
+        {
+            var posts = await UnitOfWork.Posts.GetAllAsync(null, p => p.User, p => p.Category);
+            if (posts.Count > -1)
+            {
+                return new DataResult<PostListDto>(ResultStatus.Success, new PostListDto
+                {
+                    Posts = posts,
+                    ResultStatus = ResultStatus.Success
+                });
+            }
+            return new DataResult<PostListDto>(ResultStatus.Error, Messages.Post.NotFound(isPlural: true), null);
+        }
+
+        public async Task<IDataResult<PostListDto>> GetAllByCategoryAsync(int categoryId)
+        {
+            var result = await UnitOfWork.Categories.AnyAsync(c => c.Id == categoryId);
+            if (result)
+            {
+                var posts = await UnitOfWork.Posts.GetAllAsync(p => p.CategoryId == categoryId && !p.IsDeleted && p.IsActive, p => p.User, p => p.Category);
+                if (posts.Count > -1)
+                {
+                    return new DataResult<PostListDto>(ResultStatus.Success, new PostListDto
+                    {
+                        Posts = posts,
+                        ResultStatus = ResultStatus.Success
+                    });
+                }
+                return new DataResult<PostListDto>(ResultStatus.Error, Messages.Post.NotFound(isPlural: true), null);
+            }
+            return new DataResult<PostListDto>(ResultStatus.Error, Messages.Post.NotFound(isPlural: false), null);
+        }
+
+        /* public async Task<IDataResult<PostListDto>> GetAllByDeletedAsync()
+        {
+            var posts = await UnitOfWork.Posts.GetAllAsync(p => p.IsDeleted && !p.IsActive, p => p.User, p => p.Category);
+            if (posts.Count > -1)
+            {
+                return new DataResult<PostListDto>(ResultStatus.Success, new PostListDto
+                {
+                    Posts = posts,
+                    ResultStatus = ResultStatus.Success
+                });
+            }
+            return new DataResult<PostListDto>(ResultStatus.Error, Messages.Post.NotFound(isPlural: true), null);
+        }*/
+
+        public async Task<IDataResult<PostListDto>> GetAllByNonDeletedAsync()
+        {
+            var posts = await UnitOfWork.Posts.GetAllAsync(p => !p.IsDeleted, p => p.User, p => p.Category);
+            if (posts.Count > -1)
+            {
+                return new DataResult<PostListDto>(ResultStatus.Success, new PostListDto
+                {
+                    Posts = posts,
+                    ResultStatus = ResultStatus.Success
+                });
+            }
+            return new DataResult<PostListDto>(ResultStatus.Error, Messages.Post.NotFound(isPlural: true), null);
+        }
+
+        public async Task<IDataResult<PostListDto>> GetAllByNonDeletedAndActiveAsync()
+        {
+            var posts = await UnitOfWork.Posts.GetAllAsync(p => !p.IsDeleted && p.IsActive, p => p.User, p => p.Category);
+            if (posts.Count > -1)
+            {
+                return new DataResult<PostListDto>(ResultStatus.Success, new PostListDto
+                {
+                    Posts = posts,
+                    ResultStatus = ResultStatus.Success
+                });
+            }
+            return new DataResult<PostListDto>(ResultStatus.Error, Messages.Post.NotFound(isPlural: true), null);
+        }
+
+        /* public Task<IDataResult<PostListDto>> GetAllByPagingAsync(Guid? categoryId, int currentPage = 1, int pageSize = 5, bool isAscending = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDataResult<PostListDto>> GetAllByViewCountAsync(bool isAscending, int? takeSize)
+        {
+            throw new NotImplementedException();
+        } */
+
+        public async Task<IResult> DeleteAsync(int postId, string modifiedByName)
+        {
+            var result = await UnitOfWork.Posts.AnyAsync(p => p.Id == postId);
+            if (result)
+            {
+                var post = await UnitOfWork.Posts.GetAsync(p => p.Id == postId);
+                post.IsDeleted = true;
+                post.IsActive = false;
+                post.ModifiedByName = modifiedByName;
+                post.ModifiedDate = DateTime.Now;
+                await UnitOfWork.Posts.UpdateAsync(post);
+                await UnitOfWork.SaveAsync();
+                return new Result(ResultStatus.Success, Messages.Post.Delete(post.Title));
+            }
+            return new Result(ResultStatus.Error, Messages.Post.NotFound(isPlural: false));
+        }
+
+        public async Task<IResult> HardDeleteAsync(int postId)
+        {
+            var result = await UnitOfWork.Posts.AnyAsync(p => p.Id == postId);
+            if (result)
+            {
+                var post = await UnitOfWork.Posts.GetAsync(p => p.Id == postId);
+                await UnitOfWork.Posts.DeleteAsync(post);
+                await UnitOfWork.SaveAsync();
+                return new Result(ResultStatus.Success, Messages.Post.HardDelete(post.Title));
+            }
+            return new Result(ResultStatus.Error, Messages.Post.NotFound(isPlural: false));
+        }
+
+        public async Task<IResult> UndoDeleteAsync(int postId, string modifiedByName)
+        {
+            var result = await UnitOfWork.Posts.AnyAsync(p => p.Id == postId);
+            if (result)
+            {
+                var post = await UnitOfWork.Posts.GetAsync(p => p.Id == postId);
+                post.IsDeleted = false;
+                post.IsActive = true;
+                post.ModifiedByName = modifiedByName;
+                post.ModifiedDate = DateTime.Now;
+                await UnitOfWork.Posts.UpdateAsync(post);
+                await UnitOfWork.SaveAsync();
+                return new Result(ResultStatus.Success, Messages.Post.UndoDelete(post.Title));
+            }
+            return new Result(ResultStatus.Error, Messages.Post.NotFound(isPlural: false));
+        }
+    }
+}
