2dd144f8153ac384286a4d03e21af74c83ec6d20
jox <zincir_890@hotmail.com>
2023-12-20 05:16:56 +0300

    Added Service Collection Extensions

---
+++ TRA.Services/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRA.Data.Abstract;
+using TRA.Data.Concrete;
+using TRA.Data.Concrete.EntityFramework.Contexts;
+using TRA.Entities.Concrete;
+using TRA.Services.Abstract;
+using TRA.Services.Concrete;
+
+namespace TRA.Services.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection LoadMyServices(this IServiceCollection serviceCollection, string connectionString)
+        {
+            serviceCollection.AddDbContext<TRADbContext>(options => options.UseNpgsql(connectionString));
+            serviceCollection.AddIdentity<User, Role>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 5;
+                options.Password.RequiredUniqueChars = 0;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                // User, Username, Email options
+                options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = true;
+            }).AddEntityFrameworkStores<TRADbContext>();
+            serviceCollection.Configure<SecurityStampValidatorOptions>(options =>
+            {
+                options.ValidationInterval = TimeSpan.FromMinutes(15);
+            });
+            serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>();
+            serviceCollection.AddScoped<ICategoryService, CategoryManager>();
+            serviceCollection.AddScoped<IPostService, PostManager>();
+            serviceCollection.AddScoped<ICommentService, CommentManager>();
+            return serviceCollection;
+        }
+    }
+}
