bd1cfffd7373b5d5c39df9123c777c73f6737268
jox <zincir_890@hotmail.com>
2024-01-02 04:34:02 +0300

    Added BaseController

---
+++ TRA/Controllers/BaseController.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TRA.Entities.Concrete;
+
+namespace TRA.Controllers
+{
+    public class BaseController : Controller
+    {
+        public BaseController(UserManager<User> userManager, IMapper mapper)
+        {
+            UserManager = userManager;
+            Mapper = mapper;
+        }
+
+        protected UserManager<User> UserManager { get; }
+        protected IMapper Mapper { get; }
+        protected User LoggedInUser => UserManager.GetUserAsync(HttpContext.User).Result;
+    }
+}
