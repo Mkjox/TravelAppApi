dbf801c74cc03dbc0f2f0f409404da1efda3d132
jox <zincir_890@hotmail.com>
2024-01-25 15:35:28 +0300

    Created CommentController

---
+++ TRA/Controllers/CommentController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TRA.Entities.Concrete;
+
+namespace TRA.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentController : BaseController
+    {
+        private readonly IList<Comment> _comments = new List<Comment>();
+        private readonly IMapper _mapper;
+
+        public CommentController(UserManager<User> userManager, IMapper mapper) : base(userManager, mapper)
+        {
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetComments()
+        {
+            return Ok(_comments);
+        }
+    }
+}
