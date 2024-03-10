6df4e2ac3e20c2d42c755b9f6481e86533e6b0dc
jox <zincir_890@hotmail.com>
2024-03-10 05:00:38 +0300

    Added ILikedItemRepository

---
+++ TRA.Data/Abstract/ILikedItemRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRA.Entities.Concrete;
+using TRA.Shared.Data.Abstract;
+
+namespace TRA.Data.Abstract
+{
+    public interface ILikedItemRepository : IEntityRepository<LikedItem>
+    {
+    }
+}
--- TRA.Data/Abstract/IUnitOfWork.cs
+++ TRA.Data/Abstract/IUnitOfWork.cs
@@ -11,6 +11,7 @@ namespace TRA.Data.Abstract
         IPostRepository Posts { get; }
         ICategoryRepository Categories { get; }
         ICommentRepository Comments { get; }
+        ILikedItemRepository LikedItems { get; }
         Task<int> SaveAsync();
     }
 }
