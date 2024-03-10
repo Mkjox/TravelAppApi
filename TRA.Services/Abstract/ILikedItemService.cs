fec10ab512d67aef97be9e5bbf7df7a76034137d
jox <zincir_890@hotmail.com>
2024-03-10 05:03:29 +0300

    Added ILikedItemService

---
+++ TRA.Services/Abstract/ILikedItemService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRA.Entities.Dtos;
+
+namespace TRA.Services.Abstract
+{
+    public interface ILikedItemService
+    {
+        Task<LikedItemDto> AddLikedItemAsync(LikedItemDto likedItemDto);
+        Task DeleteLikedItemAsync(int id);
+        Task<IEnumerable<LikedItemDto>> GetAllLikedItemsAsync();
+        Task<LikedItemDto> GetLikedItemByIdAsync(int id);
+    }
+}
