7384ea7be7a6c9cf1446b4e1fcce55d8a60f2ed1
jox <zincir_890@hotmail.com>
2024-03-16 02:52:29 +0300

    Added LikedItemUpdateDto

---
+++ TRA.Entities/Dtos/LikedItemUpdateDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRA.Shared.Entities.Abstract;
+
+namespace TRA.Entities.Dtos
+{
+    public class LikedItemUpdateDto : DtoGetBase
+    {
+        public bool IsLiked { get; set; } = false;
+        public int LikedCount { get; set; } = 0;
+    }
+}
