96e8bdc63bc62d59d1be0f4b1b180ff9d79c51da
jox <zincir_890@hotmail.com>
2024-03-28 12:48:24 +0300

    Adding UpdatePost Operation

--- TRA/Controllers/PostController.cs
+++ TRA/Controllers/PostController.cs
@@ -27,20 +27,20 @@ namespace TRA.Controllers
             _mapper = mapper;
         }
 
-        [HttpGet]
+        [HttpGet("getall")]
         public async Task<IActionResult> GetPosts()
         {
             return Ok(_posts);
         }
 
         [HttpPost]
-        public async Task<IActionResult> AddPost([FromBody] Post post)
+        public async Task<IActionResult> AddPost([FromBody] PostAddDto postAddDto)
         {
             if (ModelState.IsValid)
             {
-                var postAddDto = _mapper.Map<PostAddDto>(post);
+                var _postAddDto = _mapper.Map<PostAddDto>(postAddDto);
 
-                var result = await _postService.AddAsync(postAddDto, LoggedInUser.UserName, LoggedInUser.Id);
+                var result = await _postService.AddAsync(_postAddDto, LoggedInUser.UserName, LoggedInUser.Id);
 
                 if (result.ResultStatus == ResultStatus.Success)
                 {
@@ -52,7 +52,7 @@ namespace TRA.Controllers
                 }
             }
             var categories = await _categoryService.GetAllByNonDeletedAndActiveAsync();
-            post.Category = (Category)categories.Data.Categories;
+            postAddDto.Category = (Category)categories.Data.Categories;
 
             if (!ModelState.IsValid)
             {
@@ -65,8 +65,38 @@ namespace TRA.Controllers
 
 
             // _posts.Add(post);
-
             //return CreatedAtAction(nameof(GetPosts), new { id = post.Id }, post);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdatePost(int postId)
+        {
+            var postResult = await _postService.GetPostUpdateDtoAsync(postId);
+            var categoriesResult = await _categoryService.GetAllByNonDeletedAndActiveAsync();
+            if (postResult.ResultStatus == ResultStatus.Success && categoriesResult.ResultStatus == ResultStatus.Success)
+            {
+                var postUpdateDto = Mapper.Map<PostUpdateDto>(postResult.Data);
+                return Json(postUpdateDto);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost("update")]
+        public async Task<IActionResult> UpdatePost([FromBody] PostUpdateDto postUpdateDto)
+        {
+            var _postUpdateDto = _mapper.Map<PostUpdateDto>(postUpdateDto);
+            var result = _postService.UpdateAsync(_postUpdateDto, LoggedInUser.UserName);
+
+            if (result.IsCompletedSuccessfully)
+            {
+                return Ok(result);
+            }
+            else
+                return BadRequest(result);
+        }
+
     }
 }
