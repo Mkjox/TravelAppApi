012669d8b10366674abfea4f4e35edcbfe68fa43
jox <zincir_890@hotmail.com>
2024-04-02 18:55:12 +0300

    Added and Configured Operations

--- TRA/Controllers/LikedController.cs
+++ TRA/Controllers/LikedController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using TRA.Entities.Concrete;
 using TRA.Entities.Dtos;
 using TRA.Services.Abstract;
+using TRA.Shared.Utilities.Results.ComplexTypes;
 
 namespace TRA.Controllers
 {
@@ -30,18 +31,57 @@ namespace TRA.Controllers
             var result = _likedItemService.GetAllAsync();
             if (result.IsCompletedSuccessfully)
                 return Ok(result);
-            else 
+            else
                 return BadRequest(result);
         }
 
-        [HttpPost("add")]
-        public async Task<IActionResult> AddLike(LikedItem likedItem)
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] LikedItemAddDto likedItemAddDto)
         {
-            var result = _likedItemService.AddAsync(likedItem);
-            if (result.IsCompletedSuccessfully)
-                return Ok(result);
-            else 
-                return BadRequest(result);
+            var _likedItemAddDto = _mapper.Map<LikedItemAddDto>(likedItemAddDto);
+
+            var result = await _likedItemService.AddAsync(_likedItemAddDto);
+
+            if (result.ResultStatus == ResultStatus.Success)
+            {
+                return Json(new
+                {
+                    result.Message,
+                    Title = "Liked Successfully!"
+                });
+            }
+            else
+            {
+                return Json(new
+                {
+                    Result = false,
+                    Message = "There has been an error while liking the content."
+                });
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update([FromBody] LikedItemUpdateDto likedItemUpdateDto)
+        {
+            var _likedItemUpdateDto = _mapper.Map<LikedItemUpdateDto>(likedItemUpdateDto);
+            var result = await _likedItemService.UpdateAsync(_likedItemUpdateDto);
+
+            if (result.ResultStatus == ResultStatus.Success)
+            {
+                return Json(new
+                {
+                    result.Message,
+                    Title = "Unliked Successfully!"
+                });
+            }
+            else
+            {
+                return Json(new
+                {
+                    Result = false,
+                    Message = "There has been an error while unliking the content."
+                });
+            }
         }
     }
 }
