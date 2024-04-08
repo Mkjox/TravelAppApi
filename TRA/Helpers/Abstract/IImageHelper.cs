d85e744cc06b23abe95bea3dcb8222eaf4f07847
jox <zincir_890@hotmail.com>
2024-04-08 07:09:58 +0300

    Added Classes for Images

---
+++ TRA/Helpers/Abstract/IImageHelper.cs
@@ -0,0 +1,12 @@
+﻿using TRA.Entities.ComplexTypes;
+using TRA.Entities.Dtos;
+using TRA.Shared.Utilities.Results.Abstract;
+
+namespace TRA.Helpers.Abstract
+{
+    public interface IImageHelper
+    {
+        Task<IDataResult<ImageUploadedDto>> Upload(string name, IFormFile pictureFile, PictureType pictureType, string folderName = null);
+        IDataResult<ImageDeletedDto> Delete(string pictureName);
+    }
+}
