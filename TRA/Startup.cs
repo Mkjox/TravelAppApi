b86512ff9d76b470ff238771f4125494856ee89b
jox <zincir_890@hotmail.com>
2024-05-27 18:52:51 +0300

    Tried to fix a bug

--- TRA/Startup.cs
+++ TRA/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using TRA.AutoMapper.Profiles;
 using TRA.Data.Concrete.EntityFramework.Contexts;
+using TRA.Entities.Concrete;
 using TRA.Services.AutoMapper.Profiles;
 
 namespace TRA
@@ -17,6 +19,7 @@ namespace TRA
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<TRADbContext>();
+            services.AddScoped<UserManager<User>>();
             services.AddSingleton(provider => new MapperConfiguration(cfg =>
             {
                 cfg.AddProfile(new UserProfile());
@@ -24,6 +27,10 @@ namespace TRA
                 cfg.AddProfile(new CategoryProfile());
                 cfg.AddProfile(new CommentProfile());
             }).CreateMapper());
+
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<TRADbContext>()
+                .AddDefaultTokenProviders();
 
             services.AddAutoMapper(typeof(CategoryProfile), typeof(PostProfile), typeof(UserProfile), typeof(CommentProfile));
         }
