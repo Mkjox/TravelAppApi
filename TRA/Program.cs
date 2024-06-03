fc5bd3d3629818e6a5f30ec572ffb6d28087b70b
jox <zincir_890@hotmail.com>
2024-06-03 18:31:11 +0300

    Trying to fix an error

--- TRA/Program.cs
+++ TRA/Program.cs
@@ -1,34 +1,67 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using TRA.AutoMapper.Profiles;
 using TRA.Data.Concrete.EntityFramework.Contexts;
+using TRA.Entities.Concrete;
+using TRA.Services.Abstract;
+using TRA.Services.AutoMapper.Profiles;
+using TRA.Services.Concrete;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<TRADbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQL")));
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<UserManager<User>>();
+builder.Services.AddScoped<IPostService, PostManager>();
+builder.Services.AddScoped<ICategoryService, CategoryManager>();
 
-builder.Services.AddControllers();
+builder.Services.AddSingleton(provider => new MapperConfiguration(cfg =>
+{
+    cfg.AddProfile(new UserProfile());
+    cfg.AddProfile(new PostProfile());
+    cfg.AddProfile(new CategoryProfile());
+    cfg.AddProfile(new CommentProfile());
+}).CreateMapper());
+
+builder.Services.AddIdentity<User, IdentityRole>()
+    .AddEntityFrameworkStores<TRADbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddAutoMapper(typeof(CategoryProfile), typeof(PostProfile), typeof(UserProfile), typeof(CommentProfile));
+
+builder.Services.AddControllersWithViews();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "TRA API", Version = "v1" });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
 
 app.Run();
