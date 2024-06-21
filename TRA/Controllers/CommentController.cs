6f56ffa3703bcba7a680f34ca10360df1832864c
jox <zincir_890@hotmail.com>
2024-06-21 06:00:24 +0300

    Added new Operations

--- TRA/Controllers/CommentController.cs
+++ TRA/Controllers/CommentController.cs
@@ -2,9 +2,12 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using TRA.Entities.Concrete;
 using TRA.Entities.Dtos;
 using TRA.Services.Abstract;
+using TRA.Shared.Utilities.Results.Abstract;
 using TRA.Shared.Utilities.Results.ComplexTypes;
 
 namespace TRA.Controllers
@@ -14,12 +17,10 @@ namespace TRA.Controllers
     public class CommentController : BaseController
     {
         private readonly IList<Comment> _comments = new List<Comment>();
-        private readonly IMapper _mapper;
         private readonly ICommentService _commentService;
 
         public CommentController(UserManager<User> userManager, IMapper mapper, ICommentService commentService) : base(userManager, mapper)
         {
-            _mapper = mapper;
             _commentService = commentService;
         }
 
@@ -34,17 +35,17 @@ namespace TRA.Controllers
         {
             if (ModelState.IsValid)
             {
-                var commentAddDto = _mapper.Map<CommentAddDto>(comment);
+                var commentAddDto = Mapper.Map<CommentAddDto>(comment);
 
                 var result = await _commentService.AddAsync(commentAddDto);
 
                 if (result.ResultStatus == ResultStatus.Success)
                 {
-                    return Ok(new { Message = result.Message, Comment = comment });
+                    return Ok(result);
                 }
                 else
                 {
-                    return BadRequest(new { ErrorMessage = result.Message });
+                    return BadRequest(result);
                 }
             }
             var comments = await _commentService.GetAllByNonDeletedAndActiveAsync();
@@ -55,6 +56,69 @@ namespace TRA.Controllers
                 return Json(new { Result = false, Message = "Please fill all the required spaces.", Errors = errors });
             }
             return Json(new { Result = false, Message = "There has been an error adding the comment." });
+        }
+
+        [HttpPost("GetComment")]
+        public async Task<IActionResult> GetCommentById(int commentId)
+        {
+            var comment = await _commentService.GetAsync(commentId);
+            return Json(comment);
+        }
+
+        [HttpPost("GetAllComments")]
+        public async Task<IActionResult> GetAllComments()
+        {
+            var comments = await _commentService.GetAllAsync();
+            if (comments != null)
+            {
+                return Json(comments);
+            }
+
+            else
+                return Json(null);
+        }
+
+        [HttpPost("GetDeletedComments")]
+        public async Task<IActionResult> GetDeletedComments()
+        {
+            var comments = await _commentService.GetAllByDeletedAsync();
+            if (comments != null)
+                return Json(comments);
+
+            else
+                return Json(null);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetAllByNonDeletedComments()
+        {
+            var comments = await _commentService.GetAllByNonDeletedAsync();
+            if (comments != null)
+                return Json(comments);
+
+            else
+                return Json(null);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetAllByNonDeletedAndActiveComments()
+        {
+            var comments = await _commentService.GetAllByNonDeletedAndActiveAsync();
+            if (comments != null)
+                return Json(comments);
+            else
+                return Json(null);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CountComments()
+        {
+            var comments = await _commentService.CountAsync();
+            var commentResult = JsonSerializer.Serialize(comments.Data,new JsonSerializerOptions
+            {
+                ReferenceHandler = ReferenceHandler.Preserve
+            });
+            return Json(commentResult);
         }
     }
 }
