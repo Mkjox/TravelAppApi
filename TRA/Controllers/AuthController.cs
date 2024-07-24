e95c88f712136f085cd8e360aede5c7f3c1a1f7e
jox <zincir_890@hotmail.com>
2024-07-24 15:25:29 +0300

    Trying to add Auth for the app

---
+++ TRA/Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TRA.Data.Concrete.EntityFramework.Contexts;
+using TRA.Entities.Concrete;
+using TRA.Entities.Dtos;
+
+namespace TRA.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AuthController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] UserAddDto userAddDto)
+        {
+            var user = new User { UserName = userAddDto.UserName, Email = userAddDto.Email };
+            var result = await _userManager.CreateAsync(user, userAddDto.Password);
+
+            if (!result.Succeeded)
+                return BadRequest(result.Errors);
+
+            return Ok(result);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserLoginDto userLoginDto)
+        {
+            var result = await _signInManager.PasswordSignInAsync(userLoginDto.Email, userLoginDto.Password, false, false);
+
+            if (!result.Succeeded)
+                return Unauthorized();
+
+            return Ok();
+        }
+
+    }
+}
