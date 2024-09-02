52144e770b19368b88a8472a05c4f4cff1414e01
jox <zincir_890@hotmail.com>
2024-09-02 15:54:18 +0300

    Had to do this for now maybe i can fix it later

--- TRA.Shared/Entities/Abstract/DtoGetBase.cs
+++ TRA.Shared/Entities/Abstract/DtoGetBase.cs
@@ -14,7 +14,7 @@ namespace TRA.Shared.Entities.Abstract
         public virtual int CurrentPage { get; set; }
         public virtual int PageSize { get; set; }
         public virtual int TotalCount { get; set; }
-        public virtual int TotalPages => (int)Math.Ceiling(decimal.Divide(TotalCount, PageSize));
+        public virtual int TotalPages { get; set; } /*=> (int)Math.Ceiling(decimal.Divide(TotalCount, PageSize));*/
         public virtual bool ShowPrevious => CurrentPage > 1;
         public virtual bool ShowNext => CurrentPage < TotalPages;
         public virtual bool IsAscending { get; set; } = false;
